bbb0e5cef9c6d630d6b1c1fe1f2a5fbae092db53
Alvin <alvin.chen.718@gmail.com>
2024-12-21 16:07:18 -0500

    Generate static params for templates id pages

--- app/dashboard/templates/[id]/page.tsx
+++ app/dashboard/templates/[id]/page.tsx
@@ -1,8 +1,17 @@
 'use client';
 import DisplayEmailEditor from '@/components/EmailEditor/DisplayEmailEditor';
 
+import { fetchMockTemplates } from '@/mocks/apiMocks';
+import { Template } from '@/types/templates';
 interface Params {
   id: string;
+}
+
+export async function generateStaticParams() {
+  const templates: Template[] = await fetchMockTemplates();
+  return templates.map((template) => ({
+    id: template.id,
+  }));
 }
 
 const TemplatesIdPage = ({ params }: { params: Params }) => {
