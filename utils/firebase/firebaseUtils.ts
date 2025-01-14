bb914c8a3718bd335ece3198fb0eb72958dc3b04
Alvin <alvin.chen.718@gmail.com>
2025-01-14 16:11:10 -0500

    Add firebase

---
+++ utils/firebase/firebaseUtils.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCr6z-NKhfkeLGvpU2cYMndxMntBgeaZKQ',
+  authDomain: 'email-builder-db-d4d78.firebaseapp.com',
+  projectId: 'email-builder-db-d4d78',
+  storageBucket: 'email-builder-db-d4d78.firebasestorage.app',
+  messagingSenderId: '870827994059',
+  appId: '1:870827994059:web:10c3a650a0b31f6a46e44e',
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
