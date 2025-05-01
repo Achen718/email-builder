b8452e7c05609f648cf3734d394f9c21b58f3aec
Alvin <alvin.chen.718@gmail.com>
2025-05-01 16:28:52 -0400

    Fix: path typo

--- app/providers/AuthProvider.tsx
+++ app/providers/AuthProvider.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { ReactNode } from 'react';
-import { useAuthStateSync } from '@/hooks/useAuthStateSyncnc';
+import { useAuthStateSync } from '@/hooks/useAuthStateSync';
 
 export function AuthProvider({ children }: { children: ReactNode }) {
   useAuthStateSync(); // Sets up the auth state listener
